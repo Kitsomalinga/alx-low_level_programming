72b134f198a2c0bf02635c9c632428afb571bbf1
Kitso Malinga <k.s.malinga.5545@gmail.com>
2023-02-16 17:03:35 -0800

    6-size.c

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,7 +1,8 @@
 #include <stdio.h>
 
 /**
- * main - A program that prints the size of various computer types
+ * main - Prints the size of various types based on
+ * the computer it is compiled and run on..
  * Return: 0 (Success)
  */
 
